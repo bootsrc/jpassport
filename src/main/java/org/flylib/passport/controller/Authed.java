bce966877ec64d0a5d4f286fa9b7b7a9bd57b592
frank_liu_1 <liushaomingdev@163.com>
2017-08-22 00:14:32 +0800

    BaseLoginController -->  Authed   '

--- src/main/java/org/flylib/passport/controller/Authed.java
+++ src/main/java/org/flylib/passport/controller/Authed.java
@@ -5,6 +5,6 @@ package org.flylib.passport.controller;
 * @version 创建时间：2017年8月20日 上午11:49:50 
 * 类说明 
 */
-public class BaseLoginController {
+public interface Authed {
 
 }
--- src/main/java/org/flylib/passport/intercepter/LoginInterceptor.java
+++ src/main/java/org/flylib/passport/intercepter/LoginInterceptor.java
@@ -7,8 +7,9 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.flylib.passport.annotation.Auth;
 import org.flylib.passport.constant.MobResponseCode;
-import org.flylib.passport.controller.BaseLoginController;
+import org.flylib.passport.controller.Authed;
 import org.flylib.passport.model.Passport;
 import org.flylib.passport.service.LoginIntercepterService;
 import org.slf4j.Logger;
@@ -35,32 +36,37 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 			throws Exception {
 		String token = request.getParameter("token");
 		String userId = request.getParameter("userId");
-		logger.info("进入拦截器...........");
-		if (HandlerMethod.class.equals(handler.getClass())) {
-			HandlerMethod method = (HandlerMethod) handler;
-			Object controller = method.getBean();
-			printParameters(request, controller);
-			// 访问需要登录的接口
-			logger.info("访问需要登录的接口...........");
-			if (controller instanceof BaseLoginController) {
-				logger.info("QuickLoginedBaseController...........");
-				if (StringUtils.isEmpty(token)) {
-					flushError(response, MobResponseCode.TOKEN_IS_NULL, MobResponseCode.TOKEN_IS_NULL_DESC);
-					return false;
-				}
-				Passport passport = loginIntercepterService.getPassport(userId);
-
-				if (!passport.getToken().equals(token)) {
-					flushError(response, MobResponseCode.TOKEN_INVALID, MobResponseCode.TOKEN_INVALID_DESC);
-					return false;
-				}
-
-			} else {
-				flushError(response, MobResponseCode.TOKEN_EXPIRED, MobResponseCode.TOKEN_EXPIRED_DESC);
-				return false;
-			}
-		}
-
+		//
+		if(handler.getClass().isAssignableFrom(HandlerMethod.class)){
+//            Auth auth = ((HandlerMethod) handler).getMethodAnnotation(Auth.class);
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+//            Auth auth = handlerMethod.getClass().getDeclaredAnnotation(Auth.class);
+            // 被@Auth注解的
+//            if (auth != null && auth.value()) {
+           
+        	logger.info("进入拦截器...........");
+    		Object controller = handlerMethod.getBean();
+    		printParameters(request, controller);
+    		// 访问需要登录的接口	
+        	if (controller instanceof Authed) {
+        		logger.info("访问需要登录的接口(@Authed)...........");
+    			if (StringUtils.isEmpty(token)) {
+    				flushError(response, MobResponseCode.TOKEN_IS_NULL, MobResponseCode.TOKEN_IS_NULL_DESC);
+    				return false;
+    			}
+    			Passport passport = loginIntercepterService.getPassport(userId);
+    			if (StringUtils.isEmpty(passport.getUserId())) {
+    				flushError(response, MobResponseCode.TOKEN_EXPIRED, MobResponseCode.TOKEN_EXPIRED_DESC);
+    				return false;
+    			}
+    			if (!passport.getToken().equals(token)) {
+    				flushError(response, MobResponseCode.TOKEN_INVALID, MobResponseCode.TOKEN_INVALID_DESC);
+    				return false;
+    			}
+            } // 被@Auth注解的 结束
+            
+        }// 	 HandlerMethod 结束
+		
 		boolean flag = isPermission(request, response);
 		logger.info("isPermission:" + flag);
 		return flag;
