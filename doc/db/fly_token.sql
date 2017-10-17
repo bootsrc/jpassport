cf0a24ada6760468be361bec28fe9c43f2825d7f
frank_liu_1 <liushaomingdev@163.com>
2017-10-17 21:03:13 +0800

    Modify sql scripts.    save data structure and data.

--- doc/db/fly_token.sql
+++ doc/db/fly_token.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50632
 File Encoding         : 65001
 
-Date: 2017-08-27 22:20:35
+Date: 2017-10-17 20:59:58
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,3 +27,11 @@ CREATE TABLE `fly_token` (
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of fly_token
+-- ----------------------------
+INSERT INTO `fly_token` VALUES ('1', 'b2d159798edd67f252603ab0dac57e65', '1000000000', '2017-08-27 17:45:11', '2017-08-27 17:45:11');
+INSERT INTO `fly_token` VALUES ('2', 'bfad3897dfd296223ee84afd62119b3b', '1000000000', '2017-08-27 17:48:35', '2017-08-27 17:48:35');
+INSERT INTO `fly_token` VALUES ('3', '24dd032ccac322aa3ece13b598f1ae83', '1000000000', '2017-08-27 18:38:51', '2017-08-27 18:38:51');
+INSERT INTO `fly_token` VALUES ('5', '069473bedcc609b0d25d6746c11760e5', '1000000000', '2017-08-27 21:49:31', '2017-08-27 21:49:31');
--- doc/db/fly_user.sql
+++ doc/db/fly_user.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50632
 File Encoding         : 65001
 
-Date: 2017-08-27 22:20:49
+Date: 2017-10-17 20:59:41
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -30,3 +30,11 @@ CREATE TABLE `fly_user` (
   `openid` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of fly_user
+-- ----------------------------
+INSERT INTO `fly_user` VALUES ('1', '2017-08-27 17:45:11', '2017-08-27 17:45:11', '0', 'f379eaf3c831b04de153469d1bec345e', '15601623391', null, null);
+INSERT INTO `fly_user` VALUES ('2', '2017-08-27 17:48:35', '2017-08-27 17:48:35', '0', 'e10adc3949ba59abbe56e057f20f883e', '13053262189', null, null);
+INSERT INTO `fly_user` VALUES ('3', '2017-08-27 18:38:51', '2017-08-27 18:38:51', '0', 'f379eaf3c831b04de153469d1bec345e', '13052362189', null, null);
+INSERT INTO `fly_user` VALUES ('5', '2017-08-27 21:49:23', '2017-08-27 21:49:23', '0', 'e10adc3949ba59abbe56e057f20f883e', '13011111111', null, null);
